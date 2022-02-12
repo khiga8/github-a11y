70bc5d910b870122a9e5d0d9bbfd58add94b9b93
Kate Higa <khiga8@github.com>
2022-02-11 20:12:42 -0800

    Don't attempt to run background.js on non-GitHub sites

--- background.js
+++ background.js
@@ -1,3 +1,3 @@
 chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
-    chrome.tabs.executeScript(details.tabId,{file:"contentScript.js"});
-});
+  chrome.tabs.executeScript(details.tabId,{file:"contentScript.js"});
+}, {url: [{hostContains: 'github'}]});
