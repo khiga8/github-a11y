bf58eb8630a042b7fe57bf12e684141062447abf
Katie Foster <inkblotty@github.com>
2022-06-17 21:10:28 -0500

    adding a chrome panel

---
+++ devtools-feedback/devtools.js
@@ -0,0 +1,2 @@
+// Create a tab in the devtools area
+chrome.devtools.panels.create("DemoPanel", "toast.png", "panel.html", function(panel) {});
---
+++ devtools-feedback/panel.js
@@ -0,0 +1,1 @@
+document.getElementById('main').innerHTML = 'Loaded!';
