ccce348ca475074e02691901d92617552cd57c6a
Kate Higa <khiga8@github.com>
2023-01-23 20:08:34 -0500

    More loose digit rule

--- contentScript.js
+++ contentScript.js
@@ -53,7 +53,7 @@ function appendAccessibilityInfo() {
 }
 
 function invalidAltText(altText) {
-  const defaultMacOsScreenshotAltRegex = /Screen ?[S|s]hot \d{4}-\d{2}-\d{2} at \d{2} \d{2} \d{2} [A|P]M/gi;
+  const defaultMacOsScreenshotAltRegex = /Screen ?[S|s]hot \d{4}-\d{2}-\d{2} at \d+ \d{2} \d{2} [A|P]M/gi;
   const imageAltRegex = /image/i;
   return altText.match(defaultMacOsScreenshotAltRegex) || altText.match(imageAltRegex);
 }
