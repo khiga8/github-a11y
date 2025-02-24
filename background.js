4705e4a01b809da7386c1ab5bd4e145bafd2630d
Kate Higa <16447748+khiga8@users.noreply.github.com>
2025-02-23 20:04:24 -0500

    fix: background error

--- background.js
+++ background.js
@@ -1,9 +1,13 @@
-chrome.webNavigation.onHistoryStateUpdated.addListener(
-  function (details) {
-    console.log("onHistoryStateUpdated", details);
-    chrome.tabs.sendMessage(details.tabId, {
-      type: "navigation",
-    });
-  },
-  { url: [{ hostSuffix: "github.com" }] }
-);
+chrome.webNavigation.onHistoryStateUpdated.addListener((details) => {
+  chrome.tabs.sendMessage(details.tabId, {
+    type: "navigation",
+  }, () => {
+    if (chrome.runtime.lastError) {
+      // We need to handle 
+      console.warn("Error sending message:", chrome.runtime.lastError.message);
+    } else {
+      console.log("Message successfully received by content script.");
+    }
+  });
+
+}, { url: [{ hostSuffix: "github.com" }] })
